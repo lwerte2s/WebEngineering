52afa16b2cdd6e6d77b80fdf7f51d815e4998b25
lwerte2s <luwer3@googlemail.com>
2021-02-03 23:09:11 +0100

    uebungsdaten.js

---
+++ web/uebungs-navigator/src/uebungsdaten.js
@@ -0,0 +1,184 @@
+export default {
+
+    topics: [
+        {
+            name: "U1",
+            url: "../U1",
+            subtopics: [
+                {
+                    name: "WWW Erfolgsprinzipien",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U1/1-1_www.html",
+                },
+                {
+                    name: "HTTP",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U1/1-2_http.html",
+                },
+                {
+                    name: "HTML Literatur",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U1/1-3_html.html",
+                },
+                {
+                    name: "HTML Wireframe",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U1/1-4_htmlwireframe.html",
+                },
+            ]
+        },
+        {
+            name: "U2",
+            url: "../U2",
+            subtopics: [
+                {
+                    name: "CSS Selektoren und Farben",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U2/2-1_css.html",
+                },
+                {
+                    name: "CSS Positionierung",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U2/2-2_csspositionierung.html",
+                },
+                {
+                    name: "Wireframe with HTML and CSS",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U2/2-3_surveyform.html",
+                },
+            ]
+        },
+        {
+            name: "U3",
+            url: "../U3",
+            subtopics: [
+                {
+                    name: "Flexbox",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U3/3-1_flexbox.html",
+                },
+                {
+                    name: "Grid",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U3/3-2_grid.html",
+                },
+                {
+                    name: "Grid Page",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U3/3-3_gridpage.html",
+                },
+            ]
+        },
+        {
+            name: "U4",
+            url: "../U4",
+            subtopics: [
+                {
+                    name: "Funktionen",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U4/4-1_Funktionen.html",
+                },
+                {
+                    name: "Objekte",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U4/4-2_Objekte.html",
+                },
+                {
+                    name: "Fibonacci",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U4/4-3_Fibonacci.html",
+                },
+                {
+                    name: "Topsort",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U4/4-4_Topsort.html",
+                },
+            ]
+        },
+        {
+            name: "U5",
+            url: "../U5",
+            subtopics: [
+                {
+                    name: "Vorrang",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U5/5-1_Vorrang.html",
+                },
+                {
+                    name: "TopoIterate",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U5/5-2_TopoIterate.html",
+                },
+                {
+                    name: "TopoGenerator",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U5/5-3_TopoGenerator.html",
+                },
+                {
+                    name: "Proxy",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U5/5-4_Proxy.html",
+                },
+                {
+                    name: "DeepCopy",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U5/5-5_DeepCopy.html",
+                },
+
+            ]
+        },
+        {
+            name: "U6",
+            url: "../U6",
+            subtopics: [
+                {
+                    name: "Funktionen in JS",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U6/6-1_Funktionen.html",
+                },
+                {
+                    name: "Textanalyse",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U6/6-2_textalalyse.html",
+                },
+            ]
+        },
+        {
+            name: "U7",
+            url: "../U7",
+            subtopics: [
+                {
+                    name: "Performanzmessungen",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U7/7-1_Performanzmessungen.html",
+                },
+                {
+                    name: "Rednerliste",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U7/7-2_Rednerliste.html",
+                }
+            ]
+        },
+        {
+            name: "U8",
+            url: "../U8",
+            subtopics: [
+                {
+                    name: "Promises",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U8/8-1_promises.html",
+                },
+                {
+                    name: "Async/Await",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U8/8-2_async-await.html",
+                },
+                {
+                    name: "WWW-Navigator",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U8/8-3_WWW-Navigator.html",
+                }
+            ]
+        },
+        {
+            name: "U9",
+            url: "../U9",
+            subtopics: [
+                {
+                    name: "Vue Komponente",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U9/9-1/index.html",
+                }
+            ]
+        },
+        {
+            name: "U10",
+            url: "../U10",
+            subtopics: [
+                {
+                    name: "Login",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U10/login.html",
+                },
+                {
+                    name: "Registrierung",
+                    url: "http://www2.inf.h-brs.de/~lwerte2s/web/U10/register.html",
+                }
+            ]
+        },
+    ]
+
+
+};
